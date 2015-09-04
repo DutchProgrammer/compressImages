2db72161d43508c830c2bba1679efbcd6a8931b5
Danny <DutchProgrammer@users.noreply.github.com>
2015-09-04 15:50:20 +0200

    Create example.sh

---
+++ example.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IMAGESPATH='/home/user/domains/domain.com/public_html/images'
+
+#echo "Entering $IMAGESPATH"
+
+cd $IMAGESPATH
+
+for file in $(find . -iname '*.png'); do 
+
+	#echo "compressing: $file -> $file.bak"
+	optipng -silent -force -o7 $file && advpng --quiet -z4 "$file" && pngcrush -q -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time "$file" "$file.bak" && mv "$file.bak" "$file" -f
+	#echo "compressed: $file";
+done
+
+for file in $(find . -iname '*.jpg'); do 
+
+	#echo "compressing: $file"
+	jpegoptim --quiet -f --strip-all
+	#echo "compressed: $file";
+done
+
+for file in $(find . -iname '*.jpeg'); do 
+
+	#echo "compressing: $file"
+	jpegoptim --quiet -f --strip-all
+	#echo "compressed: $file";
+done
