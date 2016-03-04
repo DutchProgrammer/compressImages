5daab0358f222222a5ac01b9922c7db1580b3e0d
Danny <DutchProgrammer@users.noreply.github.com>
2016-03-04 09:41:41 +0100

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+function compressFolderImages {
+	echo "Entering $1"
+
+	cd $1
+	
+	for file in $(find . -iname '*.png'); do 
+	
+		#echo "compressing: $file -> $file.bak"
+		optipng -silent -force -o7 $file && advpng --quiet -z4 "$file" && pngcrush -q -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time "$file" "$file.bak" && mv "$file.bak" "$file" -f
+		#echo "compressed: $file";
+	done
+	
+	for file in $(find . -iname '*.jpg'); do 
+	
+		#echo "compressing: $file"
+		jpegoptim --max=90 --all-progressive --quiet -f --strip-all $file
+		#echo "compressed: $file";
+	done
+	
+	for file in $(find . -iname '*.jpeg'); do 
+	
+		#echo "compressing: $file"
+		jpegoptim --max=90 --all-progressive --quiet -f --strip-all $file
+		#echo "compressed: $file";
+	done
+}
+
 IMAGESPATH='/home/user/domains/domain.com/public_html/images'
-
-#echo "Entering $IMAGESPATH"
-
-cd $IMAGESPATH
-
-for file in $(find . -iname '*.png'); do 
-
-	#echo "compressing: $file -> $file.bak"
-	optipng -silent -force -o7 $file && advpng --quiet -z4 "$file" && pngcrush -q -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time "$file" "$file.bak" && mv "$file.bak" "$file" -f
-	#echo "compressed: $file";
-done
-
-for file in $(find . -iname '*.jpg'); do 
-
-	#echo "compressing: $file"
-	jpegoptim --max=90 --all-progressive --quiet -f --strip-all $file
-	#echo "compressed: $file";
-done
-
-for file in $(find . -iname '*.jpeg'); do 
-
-	#echo "compressing: $file"
-	jpegoptim --max=90 --all-progressive --quiet -f --strip-all $file
-	#echo "compressed: $file";
-done
+compressFolderImages $IMAGESPATH
